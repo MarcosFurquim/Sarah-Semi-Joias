e3a6a0eecad0c035dbdf066efc961ebcfc9a545f
Marcos Furquim <marcosfurquim@gmail.com>
2020-05-04 23:58:15 -0300

    navbar #1

--- app/index.php
+++ app/index.php
@@ -1,11 +1,11 @@
 <?php
-var_dump(getenv('teste'));
+//var_dump(getenv('teste'));
 ?>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="./style/cantina.css">
+    <link rel="stylesheet" href="./assets/dist/style.css">
 </head>
 <body>
     <?php require_once 'partials/nav.php' ?>
@@ -13,4 +13,5 @@ var_dump(getenv('teste'));
         
     </h1>
 </body>
+<script src="./assets/dist/script.js"></script>
 </html>
